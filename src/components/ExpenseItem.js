9aedbb8e7939f27cf11baf2f7d3e4d6935552124
Carlos Interone <carlos@mostlyclicks.com>
2021-07-10 16:45:59 -0600

    add more JSX to expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const ExpenseItem = (props) => {
-  return (
-    <div>
-    <h2>Expense Item!</h2>
-    <p>{props.text}</p>
-    </div>
-  )
-}
+	return (
+		<div>
+			<div>March 28th, 2021</div>
+			<h2>Car Insurance</h2>
+			<div>$400.00</div>
+			<p>{props.text}</p>
+		</div>
+	);
+};
 
-export default ExpenseItem
+export default ExpenseItem;
