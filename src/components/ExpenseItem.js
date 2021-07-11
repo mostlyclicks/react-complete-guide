7065c84f594babf75e26de6550732f7563ed9a02
Carlos Interone <carlos@mostlyclicks.com>
2021-07-11 08:20:55 -0600

    add props to expenseitems

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 import React from "react";
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 	return (
 		<div className='expense-item'>
-			<div>March 28th, 2021</div>
+			<div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$400.00</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-			
 		</div>
 	);
 };
