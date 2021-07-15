4be4130c526a1e37e5894d8c035f74658b8e3b36
Carlos Interone <carlos@mostlyclicks.com>
2021-07-14 21:10:36 -0600

    passing form data up to parent component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,18 @@ import './NewExpense.css'
 
 
 const NewExpense = () => {
+
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    }
+    console.log(expenseData)
+  }
+
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
     </div>
   )
 }
