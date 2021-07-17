e6689f7b8dfd865fabe87d759c688ab90a127b61
Carlos Interone <carlos@mostlyclicks.com>
2021-07-16 21:26:17 -0600

    add expensefilter and event listener

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,12 +2,14 @@ import React from "react";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter"
 
 const Expenses = (props) => {
 	const expenses = props.expenses;
 
 	return (
 		<Card className="expenses">
+			<ExpenseFilter />
 			{expenses.map((expense) => {
 				return (
 					<ExpenseItem
